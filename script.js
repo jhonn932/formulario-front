278e159db26ae0c71a93e2a9c508d1c8f16f54fc
yonatan ayala <informaticaprofesional142@gmail.com>
2025-06-24 23:32:57 -0300

    Ahora todo se ejecuta en una sola funcion

--- script.js
+++ script.js
@@ -1,104 +1,87 @@
-//Scrip para validacion de campos (prueba)
-const nombre = document.getElementById('nombre');
-const apellido = document.getElementById('apellido');
-const dni = document.getElementById('dni');
-const correo = document.getElementById('correo');
-const formulario = document.getElementById('registro');
-
-const m_error1 = document.getElementById('mensajeError1');
-const m_error2 = document.getElementById('mensajeError2');
-const m_error3 = document.getElementById('mensajeError3');
-const m_error4 = document.getElementById('mensajeError4');
-
-//Funcion loader
+//Scrip para validacion de campos
 window.addEventListener('load', () => {
     const loader = document.getElementById('loader');
     loader.classList.add('hidden');
 });
 
-function validarCampoTexto(input,errorM) {
-    const valor = input.value.trim();
-    let valido = true;
+function validarCampos(input,error) {
+    const id = document.getElementById(input);
+    const valor = id.value.trim();
+    const err = document.getElementById(error);
+    const A = true;
 
-    if (!valor || valor.length < 3) {
-        input.classList.remove("valido");
-        input.classList.add("invalido");
-        errorM.textContent = "El nombre es Invalido";
-        valido = false;
-    } else {
-        input.classList.remove("invalido");
-        input.classList.add("valido");
-        errorM.textContent = "";
+    switch (input) {
+        case 'nombre':
+        case 'apellido':    
+            if (!valor || valor.length < 3) {
+                id.classList.remove("valido");
+                id.classList.add("invalido");
+                err.textContent = "El nombre es Invalido";
+                A = false;
+            } else {
+                id.classList.remove("invalido");
+                id.classList.add("valido");
+                err.textContent = "";
+            }
+            break;
+
+        case 'dni':
+            if (!valor || !/^\d+$/.test(valor) || valor.length < 7 ) {
+                id.classList.remove("valido");
+                id.classList.add("invalido");
+                err.textContent = "El DNI es Invalido";
+                A = false;
+            } else {
+                id.classList.remove("invalido");
+                id.classList.add("valido");
+                err.textContent = "";
+            }
+            break;
+        
+        case 'correo':
+            const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if(valor.includes("@") && valor.length > 5 && regex.test(valor)) {
+                id.classList.remove("invalido");
+                id.classList.add("valido");
+                err.textContent = "";
+            } else {
+                id.classList.remove("valido");
+                id.classList.add("invalido");
+                err.textContent = "Correo Invalido";
+                A = false;
+            }
+            break;
     }
-    return valido;    
-}
-
-function validar_dni(input,error){
-    const valor = input.value.trim();
-    let valido = true;
-
-    if (!valor || !/^\d+$/.test(valor) || valor.length < 7 ) {
-        dni.classList.remove("valido");
-        dni.classList.add("invalido");
-        error.textContent = "El DNI es Invalido";
-        valido = false;
-    } else {
-        dni.classList.remove("invalido");
-        dni.classList.add("valido");
-        error.textContent = "";
-    }
-    return valido;      
-}
-
-function validar_correo(input,errorM){
-    const valor = input.value.trim();
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let valido = true;
-
-    if(valor.includes("@") && valor.length > 5 && regex.test(valor)) {
-        correo.classList.remove("invalido");
-        correo.classList.add("valido");
-        errorM.textContent = "";
-    } else {
-        correo.classList.remove("valido");
-        correo.classList.add("invalido");
-        errorM.textContent = "Correo Invalido";
-        valido = false;
-    }
-    return valido;
+    return A;
 }
 
 //input nombre listening
 nombre.addEventListener("input", () => {
-    validarCampoTexto(nombre,m_error1);
+    validarCampos('nombre','mensajeError1');
 }); 
 //input apellido listening
 apellido.addEventListener("input", () => {
-    validarCampoTexto(apellido,m_error2);
+    validarCampos('apellido','mensajeError2');
 });
 //input dni listening
 dni.addEventListener("input", () => {
-    validar_dni(dni,m_error3);
+    validarCampos('dni','mensajeError3');
 });
 //input correo listening
 correo.addEventListener("input", () => {
-    validar_correo(correo,m_error4);
+    validarCampos('correo','mensajeError4');
 });
 
 //Validacion antes de enviar los datos
-formulario.addEventListener("submit", function(e){
+const form = document.getElementById('registro');
+form.addEventListener("submit", function(e){
     e.preventDefault();
-    let Vnombre = validarCampoTexto(nombre,m_error1);
-    let Vapellido = validarCampoTexto(apellido,m_error2);
-    let Vdni = validarCampoTexto(dni,m_error3);
-    let Vcorreo = validar_correo(correo,m_error4);
 
+    validarCampos('nombre','mensajeError1');
+    validarCampos('apellido','mensajeError2');
+    validarCampos('dni','mensajeError3');
+    validarCampos('correo','mensajeError4');
 
-    if (Vnombre && Vapellido && Vdni && Vcorreo) {
-        alert("Felicidades te has registrado exitosamente!");
-        formulario.submit();
-    } else {
-        alert("Hay datos Incorrecto en el Registro!");
-        return;
-    }
+    alert("Felicidades te has registrado exitosamente!");
+    form.submit();
 });
